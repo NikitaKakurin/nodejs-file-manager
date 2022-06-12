479f703a4172750379371c5d52b93dac075f2dcc
NikitaKakurin <neyasut87@rambler.ru>
2022-06-12 17:51:30 +0300

    fix: decompress.js

--- src/commands/decompress.js
+++ src/commands/decompress.js
@@ -1,26 +1,23 @@
-import * as fsPromise from 'fs/promises';
 import fs from 'fs';
 import zlib from 'zlib';
 import { pipeline } from 'stream';
 import getFullPath from '../utils/getFullPath.js';
 import showDirectory from '../utils/showDirectory.js';
 import splitTwoArgs from '../utils/splitTwoArgs.js';
+import isFileAlreadyExist from '../utils/isFileAlreadyExist.js';
 
 async function decompress(arg) {
   // Decompress file (using Brotli algorithm)
   // decompress path_to_file path_to_destination(with filename)
-    const arrFromArg = splitTwoArgs(arg);
-    const pathToArch = arrFromArg[0];
-    const pathToTarget= arrFromArg[1];
-    const fullPathToArch = await getFullPath(pathToArch);
-    const fullPathToTarget = await getFullPath(pathToTarget);
+  const arrFromArg = splitTwoArgs(arg);
+  const pathToArch = arrFromArg[0];
+  const pathToTarget= arrFromArg[1];
+  const fullPathToArch = await getFullPath(pathToArch);
+  const fullPathToTarget = await getFullPath(pathToTarget);
     
-    try{
-        await fsPromise.access(fullPathToTarget);
-        console.log("The file already exist in " + fullPathToTarget)
-        console.log("Operation failed")
-        return;
-    } catch(err){}
+  if(await isFileAlreadyExist(fullPathToTarget)){
+    return;
+  };
 
   const readableStream = fs.createReadStream(fullPathToArch);
   const writeableStream = fs.createWriteStream(fullPathToTarget, {flag: 'w+'});
