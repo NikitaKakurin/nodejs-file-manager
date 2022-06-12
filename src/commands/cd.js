2cc9e59d708916bf54fa84f1adc7980ea93c561d
NikitaKakurin <neyasut87@rambler.ru>
2022-06-12 17:22:42 +0300

    feat: add comments to executeCd

--- src/commands/cd.js
+++ src/commands/cd.js
@@ -4,16 +4,17 @@ import getFullPath from '../utils/getFullPath.js';
 import showDirectory from '../utils/showDirectory.js';
 
 async function executeCd(pathToFolder){
-    // Go to dedicated folder from current directory 
-    // (path_to_directory can be relative or absolute)
+  // Go to dedicated folder from current directory 
+  // (path_to_directory can be relative or absolute)
     
-    const fullPath = await getFullPath(pathToFolder);
-    const stat = await fs.stat(fullPath);
-    if(stat.isFile()){
-        throw new Error();
-    }
-    currentDirectory.path = fullPath;
-    showDirectory()
+  const fullPath = await getFullPath(pathToFolder);
+  const stat = await fs.stat(fullPath);
+  if(stat.isFile()){
+    console.log('you input path to the file');
+    throw new Error();
+  }
+  currentDirectory.path = fullPath;
+  showDirectory()
 }
 
 export default executeCd;
